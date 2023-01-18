e2f286eb0ee10eead6ae376734de9ad02dc6d162
Vitalik0422 <vitaliy19980422@gmail.com>
2023-01-18 18:54:06 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,39 @@
 let elqunt = document.querySelector('.quantity');
 let elFullPrice = document.querySelector('.fullPrice');
+let elFullPriceAlert = document.querySelector('.alert')
 let a = 0;
+let c = 0;
+
 const price = document.querySelector('.price');
 
-{
-    price.addEventListener('change', updatePrice)
-    function updatePrice(e) {
-        let c;
+
+price.addEventListener('change', (e) =>{
         c = e.target.value;
+        console.log(c);
         elFullPrice.innerHTML = 'Загальна сума: ' + c * a;
+        if ( a < 1){
+            elFullPriceAlert.innerHTML = 'Добавте хоча б одну кількість товару';
+        }
 
-    }
+        else if ( a => 1){
+            elFullPriceAlert.innerHTML = '';
+        }
+});
 
-}
 
 {
 let buttonPlus = document.querySelector('.button-plus');
     buttonPlus.addEventListener('click', () => {
         a = a + 1
         elqunt.innerHTML = a;
+        elFullPrice.innerHTML = 'Загальна сума: ' + c * a;
+        if ( a < 1){
+            elFullPriceAlert.innerHTML = 'Добавте хоча б одну кількість товару';
+        }
+
+        else if ( a => 1){
+            elFullPriceAlert.innerHTML = '';
+        }
     });
 }
 
@@ -27,16 +42,23 @@ let buttonMinus = document.querySelector('.button-minus');
     buttonMinus.addEventListener('click', () =>{
         a = a - 1;
         elqunt.innerHTML = a;
+        elFullPrice.innerHTML = 'Загальна сума: ' + c * a;
         if (a < 0){
             elqunt.innerHTML = 0;
             a = 0;
             alert('Не може бути відємна кількість товару')
         }
+
+        if ( a < 1){
+            elFullPriceAlert.innerHTML = 'Добавте хоча б одну кількість товару';
+        }
+
+        else if ( a => 1){
+            elFullPriceAlert.innerHTML = '';
+        }
     }); 
 
 };
-
-
 console.log(a);
 console.log('Кількість', elqunt);
 
